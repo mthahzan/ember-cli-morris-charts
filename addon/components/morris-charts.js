d7a1530d4bc9a19a02b33ac824dd81ae75fc5a08
Sachindra <sachindra@calcey.com>
2016-03-04 15:46:36 +0530

    Added fix to prevent chart loading with empty data. (fixes tooltip bug in bar charts)

--- addon/components/morris-charts.js
+++ addon/components/morris-charts.js
@@ -6,29 +6,31 @@ default Ember.Component.extend({
   options: {},
   renderChart: function() {
     var options = this.get('options');
-    options.element = this.$().attr('id');
-    this.set('options', options);
+    if (options.data) {
+      options.element = this.$().attr('id');
+      this.set('options', options);
 
-    var type = this.get('type');
-    if (typeof type !== 'string') {
-      return false;
+      var type = this.get('type');
+      if (typeof type !== 'string') {
+        return false;
+      }
+      var chartBuilder;
+      switch (type.toLowerCase()) {
+        case 'area':
+          chartBuilder = window.Morris.Area;
+          break;
+        case 'line':
+          chartBuilder = window.Morris.Line;
+          break;
+        case 'bar':
+          chartBuilder = window.Morris.Bar;
+          break;
+        case 'donut':
+          chartBuilder = window.Morris.Donut;
+          break;
+      }
+      this.set('instance', chartBuilder(options));
     }
-    var chartBuilder;
-    switch (type.toLowerCase()) {
-      case 'area':
-        chartBuilder = window.Morris.Area;
-        break;
-      case 'line':
-        chartBuilder = window.Morris.Line;
-        break;
-      case 'bar':
-        chartBuilder = window.Morris.Bar;
-        break;
-      case 'donut':
-        chartBuilder = window.Morris.Donut;
-        break;
-    }
-    this.set('instance', chartBuilder(options));
   }.on('didInsertElement'),
   listenChanges: function() {
     this.$().html('').prop('style', false);
